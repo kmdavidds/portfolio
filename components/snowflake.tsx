05ac5092e434b6f7633c5e6a902d03ce11bb1150
kmdavidds <kmdavidds@gmail.com>
2025-08-20 07:53:50 +0700

    fix: add mounted state to Snowflake component

--- components/snowflake.tsx
+++ components/snowflake.tsx
@@ -1,22 +1,30 @@
 import { motion } from "framer-motion";
+import { useEffect, useState } from "react";
 
-export const Snowflake = ({ delay = 0, duration = 10, size = 4 }) => (
-  <motion.div
-    className="absolute text-slate-300/10 pointer-events-none"
-    initial={{ y: -100, x: Math.random() * window.innerWidth, rotate: 0 }}
-    animate={{
-      y: window.innerHeight + 100,
-      rotate: 360,
-      x: Math.random() * window.innerWidth,
-    }}
-    transition={{
-      duration,
-      delay,
-      repeat: Number.POSITIVE_INFINITY,
-      ease: "linear",
-    }}
-    style={{ fontSize: `${size}px` }}
-  >
-    ❄
-  </motion.div>
-);
+export const Snowflake = ({ delay = 0, duration = 10, size = 4 }) => {
+  const [mounted, setMounted] = useState<boolean>(false);
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+  if (!mounted) return null;
+  return (
+    <motion.div
+      className="absolute text-slate-300/10 pointer-events-none"
+      initial={{ y: -100, x: Math.random() * window.innerWidth, rotate: 0 }}
+      animate={{
+        y: window.innerHeight + 100,
+        rotate: 360,
+        x: Math.random() * window.innerWidth,
+      }}
+      transition={{
+        duration,
+        delay,
+        repeat: Number.POSITIVE_INFINITY,
+        ease: "linear",
+      }}
+      style={{ fontSize: `${size}px` }}
+    >
+      ❄
+    </motion.div>
+  );
+};
